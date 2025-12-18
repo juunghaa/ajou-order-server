0a0d3e7aba68e764f457a63df906d0342288da70
juunghaa <jun8ha@naver.com>
2025-12-18 15:02:22 +0900

    AI 메뉴 추천 API 추가-secret key 삭제

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const aiRouter = require('./routes/ai');
+app.use('/api/ai', aiRouter);
+
 // Middleware
 app.use(cors());
 app.use(express.json());
